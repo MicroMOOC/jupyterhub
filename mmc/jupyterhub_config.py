296a3ddc713c48b989fbc5af6e69210359f79e7f
Andory.wy <yuwang@micromooc.com>
2020-04-27 10:18:00 +0800

    新增mmc jupyterhub config配置文件

---
+++ mmc/jupyterhub_config.py
@@ -0,0 +1,40 @@
+from jupyterhub.mmcauth import MMCAuthenticator
+import os
+
+# c.JupyterHub.authenticator_class = DummyAuthenticator
+c.JupyterHub.authenticator_class = MMCAuthenticator
+
+# launch with docker
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+
+# we need the hub to listen on all ips when it is in a container
+c.JupyterHub.hub_ip = '0.0.0.0'
+# the hostname/ip that should be used to connect to the hub
+# this is usually the hub container's name
+c.JupyterHub.hub_connect_ip = 'jupyterhub'
+
+# pick a docker image. This should have the same version of jupyterhub
+# in it as our Hub.
+c.DockerSpawner.image = 'jupyter/base-notebook'
+
+# tell the user containers to connect to our docker network
+c.DockerSpawner.network_name = 'jupyterhub'
+
+# delete containers when the stop
+c.DockerSpawner.remove = True
+
+c.MMCAuthenticator.mmc_userinfo_url = 'https://newton-dev-samwell.micromooc.com/samwell/api/v1/user/current'
+
+# Explicitly set notebook directory because we'll be mounting a host volume to
+# it.  Most jupyter/docker-stacks *-notebook images run the Notebook server as
+# user `jovyan`, and set the notebook directory to `/home/jovyan/work`.
+# We follow the same convention.
+notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan/work'
+c.DockerSpawner.notebook_dir = notebook_dir
+
+# Mount the real user's Docker volume on the host to the notebook user's
+# notebook directory in the container
+c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
+
+c.Spawner.mem_limit = '64M'
+
